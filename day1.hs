36e1ec1a1ae969f9cec55e84d2fc7e2e1cc52ffa
Chris Oaksford <39574029+coaksford@users.noreply.github.com>
2025-12-01 00:00:32 -0600

    day 1 part 1 solved

--- day1.hs
+++ day1.hs
@@ -1,11 +1,19 @@
 main = do
   contents <- readFile "day1input.txt"
-  let start = 50
   let moves = fmap parseMove $ lines contents
-  print moves
+  let zeros = foldl applyAndCount (50, 0) moves
+  print $ snd zeros
 
 parseMove :: String -> Integer
 parseMove ('L':x) = (-1 * read x :: Integer)
 parseMove ('R':x) = (read x :: Integer)
 parseMove _ = 0
+
+applyAndCount :: (Integer, Integer) -> Integer -> (Integer, Integer)
+applyAndCount (position, zeroCount) move =
+  let
+    newPosition = mod (position + move) 100
+    count = if newPosition == 0 then zeroCount + 1 else zeroCount
+  in
+    (newPosition, count)
 
