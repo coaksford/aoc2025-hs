bb33c0e7688a54bac78be020d4cbe4b7e34c6869
Chris Oaksford <39574029+coaksford@users.noreply.github.com>
2025-11-30 23:30:50 -0600

    turn all the moves into simple integers

--- day1.hs
+++ day1.hs
@@ -4,12 +4,8 @@ main = do
   let moves = fmap parseMove $ lines contents
   print moves
 
-data DialPosition int
-data Direction = L | R
-  deriving Show
-
-parseMove :: String -> (Direction, Integer)
-parseMove ('L':x) = (L, read x :: Integer)
-parseMove ('R':x) = (R, read x :: Integer)
-parseMove _ = (L, 0)
+parseMove :: String -> Integer
+parseMove ('L':x) = (-1 * read x :: Integer)
+parseMove ('R':x) = (read x :: Integer)
+parseMove _ = 0
 
