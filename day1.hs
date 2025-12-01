ff17acd792bec14c0385cdd4e3b83a8349829dcc
Chris Oaksford <39574029+coaksford@users.noreply.github.com>
2025-11-30 23:26:16 -0600

    parse inputs lines

--- day1.hs
+++ day1.hs
@@ -1,6 +1,15 @@
 main = do
   contents <- readFile "day1input.txt"
   let start = 50
-  let moves = lines contents
+  let moves = fmap parseMove $ lines contents
   print moves
+
+data DialPosition int
+data Direction = L | R
+  deriving Show
+
+parseMove :: String -> (Direction, Integer)
+parseMove ('L':x) = (L, read x :: Integer)
+parseMove ('R':x) = (R, read x :: Integer)
+parseMove _ = (L, 0)
 
